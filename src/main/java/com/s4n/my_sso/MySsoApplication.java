9b576bef1b7e759c68ce266a532ef12ccc964ffe
saidurraahmaan <saidurraahmaan@gmail.com>
2025-05-16 12:23:59 +0600

    initialize

---
+++ src/main/java/com/s4n/my_sso/MySsoApplication.java
@@ -0,0 +1,13 @@
+package com.s4n.my_sso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySsoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySsoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/s4n/my_sso/MySsoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s4n.my_sso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySsoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
